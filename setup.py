ae9bc9df6ea2d74ee9f43f63533c52f9af6c2edf
vanessa <vanessa@vinta.com.br>
2018-05-30 01:00:14 -0300

    Update version to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'citi-wikibot',
   packages = ['wikibot'],
-  version = '0.1',
+  version = '0.2',
   description = 'CITI\'s Wiki page editing for lazy people.',
   author = 'Vanessa Barreiros',
   author_email = 'vanessa.barreiros@citi.org.br',
