08398325d894b75313b3f8a478449710d4ead1ad
vanessa <vanessa@vinta.com.br>
2018-05-30 01:44:49 -0300

    Clean project folder, update setup and Makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,14 @@
+clean:
+	@find . -name "*.pyc" -exec rm -rf {} \;
+	@find . -name "__pycache__" -delete
+
+readme:
+	# install m2r: pip install m2r
+	rm README.rst
+	m2r README.md
+
 publish:
+	make readme clean
 	python setup.py sdist
 	twine upload dist/*
-	rm -rf dist/
+	rm -fr build dist .egg citi_wikibot.egg-info
--- setup.py
+++ setup.py
@@ -1,23 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
 
-with open('README.md') as f:
+with open('README.rst', 'r') as f:
   readme = f.read()
-
-with open('LICENSE.txt') as f:
-  license = f.read()
 
 setup(
   name='citi-wikibot',
   packages=['wikibot'],
-  version='0.4.2',
+  version='0.4.9',
   description='CITI\'s Wiki page editing for lazy people.',
   long_description=readme,
-  long_description_content_type='text/markdown',
   author='Vanessa Barreiros',
   author_email='vanessa.barreiros@citi.org.br',
   url='https://github.com/citi-ufpe/citi-wikibot',
   keywords=['wiki', 'bot', 'wikibot', 'citi'],
-  license=license,
   install_requires=[
     'requests',
     'misaka'
