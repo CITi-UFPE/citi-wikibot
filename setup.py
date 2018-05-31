62dcefad923a0dd438ddf4742edad9ad12de3d11
vanessa <vanessa@vinta.com.br>
2018-05-31 19:38:28 -0300

    Add topics to setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,20 @@ with open('requirements.txt', 'r') as file:
 setup(
   name='citi-wikibot',
   packages=['wikibot'],
-  version='0.4.10',
+  version='0.4.12',
   description='CITI\'s Wiki page editing for lazy people.',
   long_description=readme,
   author='Vanessa Barreiros',
   author_email='vanessa.barreiros@citi.org.br',
   url='https://github.com/citi-ufpe/citi-wikibot',
   keywords=['wiki', 'bot', 'wikibot', 'citi'],
-  install_requires=requires
+  install_requires=requires,
+  classifiers=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Development Status :: 2 - Pre-Alpha',
+    'Topic :: Utilities'
+  ]
 )
