4c1565e067f3f1623e94b46961177e2af9b353b7
vanessa <vanessa@vinta.com.br>
2018-05-31 20:06:56 -0300

    Fix install_requires

--- setup.py
+++ setup.py
@@ -3,20 +3,22 @@ from setuptools import setup
 with open('README.rst', 'r') as f:
   readme = f.read()
 
-with open('requirements.txt', 'r') as file:
-    requires = [line.strip('\n') for line in file]
-
 setup(
   name='citi-wikibot',
   packages=['wikibot'],
-  version='0.4.12',
+  version='0.4.13',
   description='CITI\'s Wiki page editing for lazy people.',
   long_description=readme,
   author='Vanessa Barreiros',
   author_email='vanessa.barreiros@citi.org.br',
   url='https://github.com/citi-ufpe/citi-wikibot',
   keywords=['wiki', 'bot', 'wikibot', 'citi'],
-  install_requires=requires,
+  install_requires=[
+    'requests',
+    'tapioca-github',
+    'misaka',
+    'python-decouple'
+  ],
   classifiers=[
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python',
